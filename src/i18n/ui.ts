3fe921461b2580883506fba51209d6f589acba1e
JoaoMadeiraxyz <jaodisc@gmail.com>
2025-01-19 18:39:14 -0300

    feat: add language support for english and portuguese with translation to the home page and about page

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: "English",
+  pt: "Português",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.blog": "Blog",
+    "nav.about": "About",
+  },
+  pt: {
+    "nav.home": "Início",
+    "nav.blog": "Blog",
+    "nav.about": "Sobre",
+  },
+} as const;
